dc54ddade988b1e696fc60b2becfa8119197d36e
Andrew <andrew@alchemy.com>
2025-04-16 10:36:10 -0400

    use inherited impl, clean up more comments, add license
    Signed-off-by: Andrew <andrew@alchemy.com>

--- crypto/src/main/java/org/web3j/crypto/transaction/type/Transaction7702.java
+++ crypto/src/main/java/org/web3j/crypto/transaction/type/Transaction7702.java
@@ -1,11 +1,22 @@
+/*
+ * Copyright 2025 Web3 Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package org.web3j.crypto.transaction.type;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.apache.tuweni.bytes.Bytes;
 import org.web3j.crypto.AccessListObject;
 import org.web3j.crypto.AuthorizationTuple;
 import org.web3j.crypto.Sign;
@@ -14,22 +25,10 @@ import org.web3j.rlp.RlpString;
 import org.web3j.rlp.RlpType;
 import org.web3j.utils.Numeric;
 
-/**
- * Example EIP-7702 transaction class that extends EIP-1559–style transactions,
- * adding an authorizationList field.
- *
- * The EIP-7702 spec calls for:
- *   [chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit,
- *    destination, value, data, access_list, authorization_list,
- *    signature_y_parity, signature_r, signature_s]
- */
 public class Transaction7702 extends Transaction1559 implements ITransaction {
 
     private final List<AuthorizationTuple> authorizationList;
 
-    /**
-     * Minimal constructor (unsigned transaction).
-     */
     protected Transaction7702(
             long chainId,
             BigInteger nonce,
@@ -42,9 +41,7 @@ public class Transaction7702 extends Transaction1559 implements ITransaction {
             List<AccessListObject> accessList,
             List<AuthorizationTuple> authorizationList
     ) {
-        // Call the Transaction1559 constructor (which includes chainId, nonce, gasLimit, etc.)
         super(chainId, nonce, gasLimit, to, value, data, maxPriorityFeePerGas, maxFeePerGas);
-
         this.authorizationList = authorizationList;
     }
 
@@ -55,62 +52,32 @@ public class Transaction7702 extends Transaction1559 implements ITransaction {
         // gas_limit, to, value, data, access_list, authorization_list,
         // y_parity, r, s
         List<RlpType> values = new ArrayList<>();
-
-        // 0. chainId
         values.add(RlpString.create(getChainId()));
-
-        // 1. nonce
         values.add(RlpString.create(getNonce()));
-
-        // 2. max_priority_fee_per_gas
         values.add(RlpString.create(getMaxPriorityFeePerGas()));
-
-        // 3. max_fee_per_gas
         values.add(RlpString.create(getMaxFeePerGas()));
-
-        // 4. gas_limit
         values.add(RlpString.create(getGasLimit()));
-
-        // 5. to
         final String to = getTo();
         if (to != null && !to.isEmpty()) {
             values.add(RlpString.create(Numeric.hexStringToByteArray(to)));
         } else {
             values.add(RlpString.create(""));
         }
-
-        // 6. value
         values.add(RlpString.create(getValue()));
-
-        // 7. data
         byte[] dataBytes = Numeric.hexStringToByteArray(getData());
         values.add(RlpString.create(dataBytes));
-
-        // 8. access_list
-        // In Transaction1559 you might have a method getAccessList()
-        List<RlpType> accessListRlp = convertAccessListToRlp(getAccessList());
+        List<RlpType> accessListRlp = rlpAccessListRlp();
         values.add(new RlpList(accessListRlp));
-
-        // 9. authorization_list
-        // convert each AuthorizationTuple to RLP
         List<RlpType> authorizationRlp = convertAuthorizationListToRlp(authorizationList);
         values.add(new RlpList(authorizationRlp));
-
-        // If we have a signature, add y_parity, r, s:
         if (signatureData != null) {
-            // EIP-7702 uses y_parity in place of a normal "v"
-            // For EVM compatibility, we can store it similarly to how 1559 does:
             int recId = Sign.getRecId(signatureData, getChainId());
             values.add(RlpString.create(recId));  // y_parity
-
-            // r
             values.add(
                     RlpString.create(
                             org.web3j.utils.Bytes.trimLeadingZeroes(signatureData.getR())
                     )
             );
-
-            // s
             values.add(
                     RlpString.create(
                             org.web3j.utils.Bytes.trimLeadingZeroes(signatureData.getS())
@@ -118,41 +85,11 @@ public class Transaction7702 extends Transaction1559 implements ITransaction {
             );
         }
 
-        // Finally, wrap the entire list in an RlpList if your encoder expects that
         List<RlpType> wrapped = new ArrayList<>();
         wrapped.add(new RlpList(values));
         return wrapped;
     }
 
-    /**
-     * Helper to convert the EIP-2930/EIP-1559 access list to RLP.
-     */
-    private List<RlpType> convertAccessListToRlp(List<AccessListObject> accessList) {
-        List<RlpType> result = new ArrayList<>();
-        if (accessList != null) {
-            for (AccessListObject entry : accessList) {
-                // Each item is [address, [storageKeys...]]
-                List<RlpType> entryRlp = new ArrayList<>();
-                byte[] addressBytes = Numeric.hexStringToByteArray(entry.getAddress());
-                entryRlp.add(RlpString.create(addressBytes));
-
-                // storageKeys
-                List<RlpType> storageKeys = new ArrayList<>();
-                for (String sk : entry.getStorageKeys()) {
-                    storageKeys.add(RlpString.create(Numeric.hexStringToByteArray(sk)));
-                }
-                entryRlp.add(new RlpList(storageKeys));
-
-                result.add(new RlpList(entryRlp));
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Convert EIP-7702's authorization_list to RLP: each tuple is
-     * [authChainId, address, authNonce, y_parity, r, s].
-     */
     private List<RlpType> convertAuthorizationListToRlp(List<AuthorizationTuple> authList) {
         List<RlpType> result = new ArrayList<>();
         if (authList == null) {
@@ -173,7 +110,7 @@ public class Transaction7702 extends Transaction1559 implements ITransaction {
 
     @Override
     public TransactionType getType() {
-        return TransactionType.EIP7702;  // Make sure your enum has EIP7702
+        return TransactionType.EIP7702;
     }
 
     /**
